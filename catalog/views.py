7fea72fc7a3cfbcc231f06cc0c9e13df51b59aa4
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-05 18:07:47 +0300

    Написаны функции, контроллеры, которые будут обрабаывать запрос нашего пути home и contacts.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    '''Контроллер, который будет обрабаывать
+    наш запрос home.'''
+    return render(request, 'home.html')
+
+def contacts(request):
+    '''Контроллер, который будет обрабаывать
+    наш запрос contacts.'''
+    return render(request, 'contacts.html')
