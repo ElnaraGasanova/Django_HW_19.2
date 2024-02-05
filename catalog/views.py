b0a192d92dc6f5ff8b838548d4ee4fa73a3ee5d8
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-05 19:05:14 +0300

    Исправила путь к html файлам.

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+
 def home(request):
     '''Контроллер, который будет обрабаывать
     наш запрос home.'''
-    return render(request, 'home.html')
+    return render(request, 'catalog/home.html')
+
 
 def contacts(request):
     '''Контроллер, который будет обрабаывать
     наш запрос contacts.'''
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
